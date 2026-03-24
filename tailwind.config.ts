edd55c664a3e9be1e5b5e9c3b8a43c5575adadd0
Caio Santos <caiosantos.dev@outlook.com>
2026-03-24 06:41:52 -0300

    feat: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/layout/**/*.{ts,tsx}",
+  ],
+  darkMode: "class",
+  theme: { extend: {} },
+  plugins: [],
+};
+
+export default config;
