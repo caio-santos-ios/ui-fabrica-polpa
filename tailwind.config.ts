2fc8c7661e2cf6515c45a9f58172b00aecbf8d93
Caio Santos <caiosantos.dev@outlook.com>
2026-03-24 06:48:30 -0300

    fix: ajustes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,18 @@
+// import { heroui } from "@heroui/react";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-    "./src/layout/**/*.{ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Adicione esta linha para o HeroUI encontrar os componentes:
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // Recomendado para suporte a temas
   plugins: [],
 };
 
